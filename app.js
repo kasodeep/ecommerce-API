3904a893ec0c1be72dbcc4bb980d85dd2319d771
Kasodariya Deep <deepkasodariya44@gmail.com>
2023-03-04 16:23:51 +0530

    Added Docs and Few Mock Data Files

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-require('express-async-errors')
-require('dotenv').config()
+require("express-async-errors")
+require("dotenv").config()
 
 // express
-const express = require('express')
+const express = require("express")
 const app = express()
 
 // extra packages
-const cookieParser = require('cookie-parser')
-const fileUpload = require('express-fileupload')
-const cloudinary = require('cloudinary').v2
-const rateLimiter = require('express-rate-limit')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const cors = require('cors')
-const mongoSanitize = require('express-mongo-sanitize')
+const cookieParser = require("cookie-parser")
+const fileUpload = require("express-fileupload")
+const cloudinary = require("cloudinary").v2
+const rateLimiter = require("express-rate-limit")
+const helmet = require("helmet")
+const xss = require("xss-clean")
+const cors = require("cors")
+const mongoSanitize = require("express-mongo-sanitize")
 
 // documentation
-const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml")
 
 // database
-const connectToMongo = require('./db/connect')
+const connectToMongo = require("./db/connect")
 
 // routers
-const authRouter = require('./routes/authRoutes')
-const userRouter = require('./routes/userRoutes')
-const productRouter = require('./routes/productRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
-const orderRouter = require('./routes/orderRoutes')
+const authRouter = require("./routes/authRoutes")
+const userRouter = require("./routes/userRoutes")
+const productRouter = require("./routes/productRoutes")
+const reviewRouter = require("./routes/reviewRoutes")
+const orderRouter = require("./routes/orderRoutes")
 
 // middleware
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 
-app.set('trust proxy', 1)
-app.use(rateLimiter({
-    windowMs: 15 * 1000 * 60,
-    max: 60
-}))
+app.set("trust proxy", 1)
+app.use(
+    rateLimiter({
+        windowMs: 15 * 1000 * 60,
+        max: 60,
+    })
+)
 app.use(helmet())
 app.use(cors())
 app.use(xss())
@@ -46,27 +48,27 @@ app.use(mongoSanitize())
 
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
-app.use(express.static('./public'))
+// app.use(express.static("./public"))
 app.use(fileUpload({ useTempFiles: true }))
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    api_secret: process.env.CLOUD_API_SECRET,
 })
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send(`<h1>E-Commerce API</h1><br>
     <a href="/api-docs">Documentation</a>`)
 })
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 // routes
 
 // routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/reviews', reviewRouter)
-app.use('/api/v1/orders', orderRouter)
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/products", productRouter)
+app.use("/api/v1/reviews", reviewRouter)
+app.use("/api/v1/orders", orderRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
@@ -76,9 +78,11 @@ const port = process.env.PORT || 5000
 const start = async () => {
     try {
         await connectToMongo()
-        app.listen(port, () => { console.log(`Server started on port ${port}..`); })
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}..`)
+        })
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 start()
